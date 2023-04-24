66d2f8aded3c628698810e70ecbc3432c073a701
MartinPanelo <panelomartinn@hotmail.com>
2023-04-24 17:32:31 -0300

    validar input de años de registro

--- scripts.js
+++ scripts.js
@@ -1,21 +1,30 @@
-let colores = [
-"border-primary",
-"border-secondary",
-"border-success",
-"border-danger",
-"border-warning",
-"border-info"]
+let colores = ["border-primary", "border-secondary", "border-success", "border-danger", "border-warning", "border-info"];
 let paisCombo;
 let registro = [];
 let btnpais = document.getElementById("idpais");
 let dropdown = document.getElementById("dropdown");
 let paises = new Set();
 let formCantUser = document.forms["fcantuser"];
+let formregistrotiempo = document.forms["fregistrotiempo"];
 let elementosdetabla = document.getElementById("tablaElemenotos");
 const URLAPI = "https://randomuser.me/api/?results=";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+function validadUserRegistroTiempo() {
+  // controlo el filtro de años de registro >= 0
+  if (formregistrotiempo["registiempo"].value >= 0) {
+    formregistrotiempo["registiempo"].classList.add("is-valid");
+    formregistrotiempo["registiempo"].classList.remove("is-invalid");
+    filtrarporañio();
+
+    // cargarUser();  // en caso de ser valido
+  } else {
+    formregistrotiempo["registiempo"].classList.add("is-invalid");
+    formregistrotiempo["registiempo"].classList.remove("is-valid");
+  }
+  return false;
 }
 function validadCantUser() {
   // controlo que la cantidad de usuarios sea mayor o igual a uno
@@ -23,7 +32,7 @@ function validadCantUser() {
     formCantUser["cantUser"].classList.add("is-valid");
     formCantUser["cantUser"].classList.remove("is-invalid");
     CargaDeUsers();
-   
+
     // cargarUser();  // en caso de ser valido
   } else {
     formCantUser["cantUser"].classList.add("is-invalid");
@@ -31,7 +40,6 @@ function validadCantUser() {
   }
   return false;
 }
-
 
 function popover() {
   var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
@@ -41,13 +49,13 @@ function popover() {
 }
 
 function listarusers(regis) {
-
-
   let posM = 0;
   let posF = 0;
-  
-  let cantFilas =Math.max( regis.filter(item => (item.genero == "male")).length,
-     regis.filter(item => (item.genero == "female")).length)
+
+  let cantFilas = Math.max(
+    regis.filter((item) => item.genero == "male").length,
+    regis.filter((item) => item.genero == "female").length
+  );
 
   elementosdetabla.innerHTML = ``;
   for (let i = 0; i < cantFilas; i++) {
@@ -57,13 +65,9 @@ function listarusers(regis) {
   </tr>`;
   }
 
-  
   regis.forEach((UnaPersona) => {
-   
-    
     if (UnaPersona.genero == "male") {
-      document.getElementById("M+" + posM).innerHTML =
-        `
+      document.getElementById("M+" + posM).innerHTML = `
         <div id="${UnaPersona.id}" class=" d-flex align-items-center">
           <img
             data-bs-trigger="hover focus"
@@ -91,7 +95,8 @@ function listarusers(regis) {
           <div class="ms-3">
           <button type="button" class="btn btn-danger " onclick="borrarUser('${UnaPersona.id}')">Borrar</button>
           </div>
-        </div>`; + document.getElementById("M+" + posM).innerHTML;
+        </div>`;
+      +document.getElementById("M+" + posM).innerHTML;
       posM += 1;
     } else {
       document.getElementById("F+" + posF).innerHTML += `
@@ -125,7 +130,6 @@ function listarusers(regis) {
     }
   });
   popover();
-
 }
 async function CargaDeUsers() {
   registro.length = 0;
@@ -154,73 +158,55 @@ async function CargaDeUsers() {
   /////////////////////////////////////////////////////
 }
 
- 
-function filtrarporañio(pais=paisCombo) {
-  let formregistrotiempo = document.forms["fregistrotiempo"];
-  let anio =formregistrotiempo["registiempo"].value
+function filtrarporañio(pais = paisCombo) {
+  //let formregistrotiempo = document.forms["fregistrotiempo"];
+  let anio = formregistrotiempo["registiempo"].value;
   let regitroFiltroanio = [];
 
-
-  regitroFiltroanio = registro.filter((persona) => persona.anioRegistro > anio && 
-  (persona.pais == pais || pais == undefined) );
+  regitroFiltroanio = registro.filter((persona) => persona.anioRegistro > anio && (persona.pais == pais || pais == undefined));
   listarusers(regitroFiltroanio);
   return false;
 }
 
-function borrarUser(id){
-
-
-  registro = registro.filter(persona => persona.id != id);
+function borrarUser(id) {
+  registro = registro.filter((persona) => persona.id != id);
   filtrarporañio();
   cargarCombo();
 }
 
-
-function cargarCombo(){
-
+function cargarCombo() {
   paises.clear();
   dropdown.innerHTML = `<li><button class="dropdown-item" onclick="filtrarporcombo()">Ver todos</<button></li>
   <li><hr class="dropdown-divider"></li>`;
 
   registro.forEach((UnaPersona) => {
-
-   paises.add( UnaPersona.pais);
-
-  })
+    paises.add(UnaPersona.pais);
+  });
   paises.forEach((pais) => {
-    dropdown.innerHTML += `<li><button class="dropdown-item" onclick="filtrarporcombo('${pais}')">${pais}</<button></li>`
-  
-  
-})
-
+    dropdown.innerHTML += `<li><button class="dropdown-item" onclick="filtrarporcombo('${pais}')">${pais}</<button></li>`;
+  });
 }
 
-
-function filtrarporcombo(pais){
+function filtrarporcombo(pais) {
   paisCombo = pais;
 
-  paisCombo === undefined ? btnpais.innerHTML = "Filtar por pais" : btnpais.innerHTML = paisCombo;
-  
+  paisCombo === undefined ? (btnpais.innerHTML = "Filtar por pais") : (btnpais.innerHTML = paisCombo);
+
   filtrarporañio(paisCombo);
 }
 
-
-function ordenarPorApellidoYNombreALosUsuarios(cuallista){
-
+function ordenarPorApellidoYNombreALosUsuarios(cuallista) {
   registro.sort((a, b) => {
-
-
-
     if (a.nombre < b.nombre && a.genero == cuallista) {
       return -1;
     }
-    if (a.nombre > b.nonbre  && a.genero == cuallista) {
+    if (a.nombre > b.nonbre && a.genero == cuallista) {
       return 1;
     }
-  
+
     // names must be equal
     return 0;
-  })
+  });
 
   filtrarporañio();
 }
